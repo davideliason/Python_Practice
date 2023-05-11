d2c3992036b134fcbebe206db51ddaf1f4966371
David Eliason <davethemaker@gmail.com>
2023-05-10 18:41:34 -0700

    start main function

--- python_project_sandbox/basic_level_projects/zippitydoodah.py
+++ python_project_sandbox/basic_level_projects/zippitydoodah.py
@@ -6,5 +6,12 @@ import random
 NUM_DIGITS = 3
 MAX_GUESSES = 10
 
+
 def main():
-  
+   print("ZippityDooDah: a guessing game. You get 10 guesses. Zippity: number is right but in wrong spot. Doo is right number in right spot. Dah is not right number or right spot")
+
+   while True:
+         secretNum = getSecretNum()
+         print("I have a number")
+         print(' You have {} guesses left!'.format(MAX_GUESSES))
+main()  
