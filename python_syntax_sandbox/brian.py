1c094c3dbc28fe1a4cf2058b3542ed837693867b
David Eliason <davethemaker@gmail.com>
2023-07-11 14:38:38 -0700

    program to obtain input name for a greeting

--- python_syntax_sandbox/brian.py
+++ python_syntax_sandbox/brian.py
@@ -1,2 +1,2 @@
-#!/Users/davide/anaconda3/bin/python3
+#! /usr/bin/env python3
 print("hello " + "The Life of Brian" + " is funny")
---
+++ python_syntax_sandbox/hello.py
@@ -0,0 +1,9 @@
+# a basic program that says hello and asks for name
+
+print('Hello, world!')
+print('What is your name?') # ask for user name
+
+myName = input()
+print('Nice to meet you, ' + myName)
+print('The length of your name is ')
+print(len(myName))
