19e37a3edeb4fd236524612bb674cc0da379134a
David Eliason <davethemaker@gmail.com>
2023-07-20 16:39:12 -0700

    Create the allGuests dictionary containing the guest values of name and various snacks they will bring

---
+++ python_project_sandbox/basic_level_projects/picnic_count.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# This program uses a dictionary, containing other dictionaries, to calculate the total number of an item being brought by all the guests.
+
+allGuest = {'Alice': {'apples': 5, 'pretzels': 12}, 
+           'Bob': {'ham sandwiches': 3, 'apples': 2}}
+
+print(allGuest['Alice']) 
