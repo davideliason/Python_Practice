7652500d45d54af92902f1f31af06f35ba0a1e37
David Eliason <davethemaker@gmail.com>
2023-07-20 17:22:34 -0700

    define totalBought function to count the snack totals as given as an argument

--- python_project_sandbox/basic_level_projects/picnic_count.py
+++ python_project_sandbox/basic_level_projects/picnic_count.py
@@ -3,8 +3,15 @@
 # This program uses a dictionary, containing other dictionaries, to calculate the total number of an item being brought by all the guests.
 
 allGuest = {'Alice': {'apples': 5, 'pretzels': 12}, 
-           'Bob': {'ham sandwiches': 3, 'apples': 2}}
+           'Bob': {'ham sandwiches': 3, 'apples': 2},
+           'Carol': {'cups': 3, 'apple pies':1}}
 
-print(allGuest['Alice'])
-print("this is new")
- 
+def getItem(dict, item):
+    totalNumOfItem = 0 # sum of specific item by all persons
+    for k, v in dict.items():
+        totalNumOfItem =totalNumOfItem + v.get(item, 0) # get method returns 0 if item does not exist as a key)
+    return totalNumOfItem
+
+print('Number of things being brought: ')
+print(' - Apples       ' + str(getItem(allGuest, 'apples')))
+
