7f5e879512658dd74a98b560a0712bbfb78a55b5
David Eliason <davethemaker@gmail.com>
2024-05-22 18:54:06 -0700

    use if/elif/else loop to verify radius value is as user expected, if so, then provide area value

--- python_project_sandbox/circleArea.py
+++ python_project_sandbox/circleArea.py
@@ -7,3 +7,11 @@ r = float(input("what is circle radius: "))
 # make sure this is right
 print("you wrote "+str(r)+" - is that right?")
 
+a = input(" y or n ")
+if a == "y":
+    area = pi * r **2
+    print("The area is "+str(area) + "!")
+elif a == "n":
+   print("oops")
+else:
+    print("okay")
