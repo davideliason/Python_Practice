c5933039c016403e2b56dbd076aeeac3e4367d51
David Eliason <davethemaker@gmail.com>
2024-05-22 18:47:35 -0700

    imported math module and specifically the pi function within. Asked user for circle radius value and printed that to the stdout

---
+++ python_project_sandbox/circleArea.py
@@ -0,0 +1,9 @@
+#import math module and fx
+from math import pi
+
+#user: what is circle radius?
+r = float(input("what is circle radius: "))
+
+# make sure this is right
+print("you wrote "+str(r)+" - is that right?")
+
