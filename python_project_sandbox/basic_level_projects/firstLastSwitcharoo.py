72bf6d257e452284d99243d4180fe84b3639f2e7
David Eliason <davethemaker@gmail.com>
2024-05-22 19:09:27 -0700

    rm script

--- python_project_sandbox/basic_level_projects/firstLastSwitcharoo.py
+++
@@ -1,7 +0,0 @@
-# input first name
-firstName=input("What is your first name?")
-# last name
-lastName=input("What is your last name?")
-# let's check this
-print(str(firstName) + " " + str(lastName))
-
