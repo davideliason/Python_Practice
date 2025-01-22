1f18c75c0965cde35f58ce73b2e1e3a2d64e7baf
David Eliason <davethemaker@gmail.com>
2025-01-22 13:54:34 -0800

    add a list with three values, print out, then change one value. Creae a tuple with three values, list them out.  Create a dictionary with three key-value pairs and print out the valus by referencing the key value

---
+++ python_project_sandbox/basic_level_projects/data_type_collections.py
@@ -0,0 +1,37 @@
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+# change val
+myFruitList[2] = "orange"
+
+print(myFruitList)
+
+# tuple
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+#DICTIONARY
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+# accessing dict data
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
+
