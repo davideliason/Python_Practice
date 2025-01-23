aee93f391671b50d99f6addfffc55aedfdae084f
David Eliason <davethemaker@gmail.com>
2025-01-23 14:00:18 -0800

    import module and then use that module to create a random number

--- python_project_sandbox/basic_level_projects/while-loop.py
+++ python_project_sandbox/basic_level_projects/while-loop.py
@@ -1,3 +1,10 @@
 
 print("Welcome to Guess the Number!")
 print("The rules are simple. I will think of a number, and you will try to guess it.")
+
+import random
+
+number = random.randint(1,10)
+
+isGuessRight = False
+
